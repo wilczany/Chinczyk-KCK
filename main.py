b16f302a70f4f5c74201ab0a23ee3f9c6cd630d8
wilczany <wilczewski.2407@gmail.com>
2023-11-08 13:58:58 +0100

    Utworzenie menu z npyscreen

--- main.py
+++ main.py
@@ -6,30 +6,27 @@ from time import sleep
 
 import src.controller.keyboardHandler as keyhandler
 
+import src.controller.view.menu as menu
+
+
 if __name__ == "__main__":
-    # mymap = se.Map(background=" ")  # defines mymap as a map as big as the terminal window with the background " "
-    # mytext = se.Text("Hello world")  # defines a text as "Hello world"
-    # myrectangle = se.Square(height=5, width=6,
-    #                         char="#")  # defines a rectangle width height 5, width 6 and the character "#"
-    # myframe = se.Frame(height=7, width=8, corner_chars=["┌", "┐", "└", "┘"], horizontal_chars=["─", "─"],
-    #                    vertical_chars=["│", "│"])  # defines a frame see scrap_engine.Frame
+    # view = GameView(3)
+    # view.move_from_base(0, 0)
+    # sleep(2)
+    # view.move_from_base(1, 1)
+    # view.move_from_base(2, 2)
+    # view.move_from_base(0, 3)
     #
-    # mytext.add(mymap, 0, 0)  # adds mytext to (0|0)
-    # myrectangle.add(mymap, 2, 2)  # adds myrectangle to (2|2)
-    # myframe.add(mymap, 1, 1)  # adds myframe to (1|1)
+    # tab = [0, 3]
+    #
+    # key_in = keyhandler.KeyboardListener(view)
+    # dec = key_in.listen(tab)
+    #
+    # view.move_to_base(dec)
 
-    # mymap.show()  # now a frame with a rectangle and a text above it should be shown
+    m = menu.Menu()
+    m.run()
 
-    view = GameView(3)
-    view.move_from_base(0, 0)
-    sleep(2)
-    view.move_from_base(1, 1)
-    view.move_from_base(2, 2)
-    view.move_from_base(0, 3)
+    plrs = menu.GameParams.players_count = 3
+    view = GameView(plrs)
 
-    tab = [0, 3]
-
-    key_in = keyhandler.KeyboardListener(view)
-    dec = key_in.listen(tab)
-
-    view.move_to_base(dec)
--- src/controller/view/menu.py
+++ src/controller/view/menu.py
@@ -1,25 +1,56 @@
-import npy  screen
+import curses
+
+import npyscreen
 
 
-class App(npyscreen.NPSApp):
-    def main(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F = npyscreen.Form(name="Welcome to Npyscreen", )
-        t = F.add(npyscreen.TitleText, name="Text:", )
-        fn = F.add(npyscreen.TitleFilename, name="Filename:")
-        fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        dt = F.add(npyscreen.TitleDateCombo, name="Date:")
-        s = F.add(npyscreen.TitleSlider, out_of=12, name="Slider")
-        ml = F.add(npyscreen.MultiLineEdit,
-                   value="""try typing here!\nMutiline text, press ^R to reformat.\n""",
-                   max_height=5, rely=9)
-        ms = F.add(npyscreen.TitleSelectOne, max_height=4, value=[1, ], name="Pick One",
-                   values=["Option1", "Option2", "Option3"], scroll_exit=True)
-        ms2 = F.add(npyscreen.TitleMultiSelect, max_height=-2, value=[1, ], name="Pick Several",
-                    values=["Option1", "Option2", "Option3"], scroll_exit=True)
+class GameParams:
+    players_count = int()
 
-        # This lets the user interact with the Form.
-        F.edit()
 
-        print(ms.get_selected_objects())
+class Menu(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.registerForm('MAIN', MainForm())
+        self.registerForm('PLAYERS_COUNT', PlayerForm())
+        # self.registerForm('GAME', GameForm())
+
+    def onCleanExit(self):
+        curses.endwin()
+
+class MainForm(npyscreen.ActionFormMinimal):
+    choices = ['Graj', 'Wyjdz']
+
+    # choises = ['Graj lokalnie', 'Graj przez siec', 'Wyjdz']
+
+    def create(self):
+
+        self.add(npyscreen.TitleText, name='Witaj w grze Chińczyk', editable=False)
+        self.choice = self.add(npyscreen.TitleSelectOne, max_height=4, name='Wybierz opcje',
+                               values=self.choices)
+
+    def on_ok(self):
+        if self.choice.get_selected_objects()[0] == 'Graj':
+            self.parentApp.switchForm('PLAYERS_COUNT')
+        else:
+            self.parentApp.setNextForm(None)
+
+
+class PlayerForm(npyscreen.ActionFormV2, npyscreen.Popup):
+
+    def create(self):
+        self.add(npyscreen.TitleText, name="Podaj liczbe graczy", editable=False)
+        self.count = self.add(npyscreen.Slider, out_of=4, lowest=2, value=2, name="Gracze")
+
+    def on_ok(self):
+        GameParams.players_count = int(self.count.value)
+        self.parentApp.setNextForm(None)
+
+    def on_cancel(self):
+        self.parentApp.switchForm('MAIN')
+
+
+class GameForm(npyscreen.ActionFormV2):
+
+    def create(self):
+        self.names = []
+        for i in range(self.parentApp.s):
+            self.names.append(self.add(npyscreen.TitleText, name="Podaj imie gracza " + str(i + 1)))
